b34005cfae2d09a4415c36f7abdeeaac905bec63
xmn880819 <xmn880819@tamu.edu>
2019-02-12 17:17:37 -0600

    Create 136_Single Number.java

---
+++ 136_Single Number.java
@@ -0,0 +1,10 @@
+O(n)+O(1)
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n = 0;
+        for (int i =0; i< nums.length; i++){
+            n = n^nums[i];
+        }
+        return n;
+    }
+}
