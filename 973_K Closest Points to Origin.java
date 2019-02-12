13e99d592a5d524beb5d7c43eb09e10fb656a9c3
xmn880819 <xmn880819@tamu.edu>
2019-02-11 19:14:39 -0600

    Update 973_K Closest Points to Origin.java

--- 973_K Closest Points to Origin.java
+++ 973_K Closest Points to Origin.java
@@ -1,24 +1,26 @@
+Input: points = [[3,3],[5,-1],[-2,4]], K = 2
+Output: [[3,3],[-2,4]]
+(The answer [[-2,4],[3,3]] would also be accepted.)
+
+Time Complexity O(n * logK)
+***************************************************************************************
 class Solution {
     public int[][] kClosest(int[][] points, int K) {
-        if (points == null || points.length == 0) {
-            return new int[0][0];
-        }
-        Queue<int[]> queue = new PriorityQueue<>(K, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] p, int[] q) {
-                return (int)(Math.pow(q[0], 2) + Math.pow(q[1], 2) - (Math.pow(p[0], 2) + Math.pow(p[1], 2)));
+        int[][] res = new int[K][2];
+        PriorityQueue<int[]> pq = new PriorityQueue<>(K, new Comparator<int[]>(){
+             public int compare(int[] o1, int[] o2){
+                return (int) ((Math.pow(o2[0], 2) + Math.pow(o2[1], 2)) - (Math.pow(o1[0], 2) + Math.pow(o1[1], 2))); //降序排列
             }
         });
-        int[][] ans = new int[K][2];
-        for (int i = 0; i < points.length; i++) {
-            queue.offer(points[i]);
-            if (queue.size() > K) {
-                queue.poll();
+        for (int i = 0; i < points.length; i++){
+            pq.offer(points[i]);  //将点放进pq里
+            if (pq.size() > K){   //大于K的扔出去
+                pq.poll();
             }
         }
-        for (int i = 0; i < K; i++) {
-            ans[i] = queue.poll();
+        for (int j = 0; j < K; j++){  //把pq里的元素放进res里
+            res[j] = pq.poll();
         }
-        return ans;
+        return res;
     }
 }
