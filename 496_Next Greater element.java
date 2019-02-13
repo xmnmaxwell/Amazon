944fd02578f147356bb70a9a32c607985e304ae6
xmn880819 <xmn880819@tamu.edu>
2019-02-12 21:23:42 -0600

    Create 496_Next Greater element.java

---
+++ 496_Next Greater element.java
@@ -0,0 +1,30 @@
+Input: nums1 = [4,1,2], nums2 = [1,3,4,2].
+Output: [-1,3,-1]
+Explanation:
+    For number 4 in the first array, you cannot find the next greater number for it in the second array, so output -1.
+    For number 1 in the first array, the next greater number for it in the second array is 3.
+    For number 2 in the first array, there is no next greater number for it in the second array, so output -1.
+******************************************************************************************
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] res = new int[nums1.length];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums2.length; i++){
+            map.put(nums2[i], i);
+        }
+        
+        for (int j = 0; j < nums1.length; j++){
+            int index = map.get(nums1[j]);
+            while(index < nums2.length){
+                if (nums2[index] > nums1[j]){
+                    res[j] = nums2[index];
+                    break;
+                } else {
+                    index++;
+                }
+                if(index == nums2.length) res[j] = -1;
+            }
+        }
+        return res;
+    }
+}
