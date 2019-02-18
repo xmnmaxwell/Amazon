f3d51eb7a5af9c847bd4c42e1a50783a65bf5c42
xmn880819 <xmn880819@tamu.edu>
2019-02-18 13:08:01 -0600

    Create 572_subtree of another tree.java

---
+++ 572_subtree of another tree.java
@@ -0,0 +1,29 @@
+Given tree s:
+
+     3
+    / \
+   4   5
+  / \
+ 1   2
+Given tree t:
+   4 
+  / \
+ 1   2
+ O(MXN)
+ ******************************************************************
+public class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null) return false;
+        if (isSame(s, t)) return true;
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+    
+    private boolean isSame(TreeNode s, TreeNode t) {
+        if (s == null && t == null) return true;
+        if (s == null || t == null) return false;
+        
+        if (s.val != t.val) return false;
+        
+        return isSame(s.left, t.left) && isSame(s.right, t.right);
+    }
+}
