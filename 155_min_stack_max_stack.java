c368dc381021b8030d2285f6bb84a591488390c7
xmn880819 <xmn880819@tamu.edu>
2019-02-18 11:26:58 -0600

    Create 155_min_stack_max_stack.java

---
+++ 155_min_stack_max_stack.java
@@ -0,0 +1,76 @@
+O(1) for each operation, O(n) space. For popMax, it is O(n) time compelxity
+class MinStack {
+    //思想就是建两个栈，一个是正常的操作，另一个是存放实时的最小值
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<Integer>();
+        minStack = new Stack<Integer>();
+    }
+    
+    public void push(int x) {
+        stack.push(x);
+        if(minStack.isEmpty()){
+            minStack.push(x);
+        } else {
+            minStack.push(Math.min(x, minStack.peek()));
+        }
+    }
+    
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+****************************************************************************
+class MaxStack {
+    Stack<Integer> stack;
+    Stack<Integer> maxStack;
+    /** initialize your data structure here. */
+    public MaxStack() {
+        stack = new Stack<Integer>();
+        maxStack = new Stack<Integer>();
+    }
+    
+    public void push(int x) {
+        stack.push(x);
+        if (maxStack.isEmpty()){
+            maxStack.push(x);   
+        }
+        else {
+            maxStack.push(Math.max(x, maxStack.peek()));
+        }
+    }
+    
+    public int pop() {
+        maxStack.pop();
+        return stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int peekMax() {
+        return maxStack.peek();
+    }
+    
+    public int popMax() {
+        int max = maxStack.peek();
+        Stack<Integer> temp = new Stack();
+        while (top() != max) temp.push(pop());
+        pop();
+        while (!temp.isEmpty()) push(temp.pop());
+        return max;
+    }
+}
