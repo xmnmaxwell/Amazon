6db8271031810284b91e1cf3e89acc0b75becce7
xmn880819 <xmn880819@tamu.edu>
2019-02-19 13:29:42 -0600

    Update 79_word search.java

--- 79_word search.java
+++ 79_word search.java
@@ -49,3 +49,47 @@ public class Solution {
         return false;
     }
 }
+*************************************************************************************************************************************
+212. pring all 
+I make my DFS with a tire but a word. The Trie is formed by all the words in given words. Then during the DFS, for each current formed word, 
+I check if it is in the Trie.
+public class Solution {
+    Set<String> res = new HashSet<String>();
+    
+    public List<String> findWords(char[][] board, String[] words) {
+        Trie trie = new Trie();
+        for (String word : words) {
+            trie.insert(word);
+        }
+        
+        int m = board.length;
+        int n = board[0].length;
+        boolean[][] visited = new boolean[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dfs(board, visited, "", i, j, trie);
+            }
+        }
+        
+        return new ArrayList<String>(res);
+    }
+    
+    public void dfs(char[][] board, boolean[][] visited, String str, int x, int y, Trie trie) {
+        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) return;
+        if (visited[x][y]) return;
+        
+        str += board[x][y];
+        if (!trie.startsWith(str)) return;
+        
+        if (trie.search(str)) {
+            res.add(str);
+        }
+        
+        visited[x][y] = true;
+        dfs(board, visited, str, x - 1, y, trie);
+        dfs(board, visited, str, x + 1, y, trie);
+        dfs(board, visited, str, x, y - 1, trie);
+        dfs(board, visited, str, x, y + 1, trie);
+        visited[x][y] = false;
+    }
+}
