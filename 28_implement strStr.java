450cee7936157ed3b44a9cd9f1e8eec4d13197ab
xmn880819 <xmn880819@tamu.edu>
2019-02-20 14:37:40 -0600

    Update 28_implement strStr.java

--- 28_implement strStr.java
+++ 28_implement strStr.java
@@ -1,6 +1,7 @@
 Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 Input: haystack = "hello", needle = "ll"
 Output: 2
+Go over the original string, compare each element as a start 
 ********************************************************************************
 O(mn)
 class Solution {
@@ -8,11 +9,11 @@ class Solution {
         if (haystack == null || needle == null) return -1;
         if (needle.length() == 0) return 0;
         for (int i = 0; i < haystack.length(); i++){
-           if (i+needle.length()>haystack.length()) return -1;
+           if (i+needle.length()>haystack.length()) return -1; // if the length is not correct, return
             int m = i;
                 for (int j = 0; j< needle.length(); j++){
-                    if (haystack.charAt(m) == needle.charAt(j)){
-                        if (j == needle.length() -1){
+                    if (haystack.charAt(m) == needle.charAt(j)){ // keep searching if char is the same, return if not
+                        if (j == needle.length() -1){ // if match, compare the length, if true return index.
                             return i;
                         } 
                         m++;
