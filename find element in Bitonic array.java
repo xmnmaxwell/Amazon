63216515d0ab2e9e01dc416eac022fc544b32489
xmn880819 <xmn880819@tamu.edu>
2019-02-19 18:21:38 -0600

    Create find element in Bitonic array.java

---
+++ find element in Bitonic array.java
@@ -0,0 +1,66 @@
+static int ascendingBinarySearch(int arr[], int low, int high, int key) { 
+        while (low <= high) { 
+            int mid = low + (high - low) / 2; 
+            if (arr[mid] == key) { 
+                return mid; 
+            } 
+            if (arr[mid] > key) { 
+                high = mid - 1; 
+            } else { 
+                low = mid + 1; 
+            } 
+        } 
+        return -1; 
+    } 
+  
+// Function for binary search in descending part of array  
+    static int descendingBinarySearch(int arr[], int low, int high, int key) { 
+        while (low <= high) { 
+            int mid = low + (high - low) / 2; 
+            if (arr[mid] == key) { 
+                return mid; 
+            } 
+            if (arr[mid] < key) { 
+                high = mid - 1; 
+            } else { 
+                low = mid + 1; 
+            } 
+        } 
+        return -1; 
+    } 
+  
+// finding bitonic point  
+    static int findBitonicPoint(int arr[], int n, int l, int r) { 
+        int mid; 
+  
+        mid = (r + l) / 2; 
+        if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) { 
+            return mid; 
+        } else { 
+            if (arr[mid] > arr[mid - 1] && arr[mid] < arr[mid + 1]) { 
+                findBitonicPoint(arr, n, mid, r); 
+            } else { 
+                if (arr[mid] < arr[mid - 1] && arr[mid] > arr[mid + 1]) { 
+                    findBitonicPoint(arr, n, l, mid); 
+                } 
+            } 
+        } 
+        return mid; 
+    } 
+  
+// Function to search key in bitonic array  
+    static int searchBitonic(int arr[], int n, int key, int index) { 
+        if (key > arr[index]) { 
+            return -1; 
+        } else if (key == arr[index]) { 
+            return index; 
+        } else { 
+            int temp = ascendingBinarySearch(arr, 0, index - 1, key); 
+            if (temp != -1) { 
+                return temp; 
+            } 
+  
+            // Search in right of k  
+            return descendingBinarySearch(arr, index + 1, n - 1, key); 
+        } 
+    } 
