fe6b72e2fbf54f1dc1302aaab173e7f6641d9357
xmn880819 <xmn880819@tamu.edu>
2019-02-19 18:41:12 -0600

    Update 268_missing number.java

--- 268_missing number.java
+++ 268_missing number.java
@@ -19,3 +19,14 @@ class Solution {
         return missing;
     }
 }
+*****************************************************************
+public int missingNumber(int[] nums) { //binary search
+    Arrays.sort(nums);
+    int left = 0, right = nums.length, mid= (left + right)/2;
+    while(left<right){
+        mid = (left + right)/2;
+        if(nums[mid]>mid) right = mid;
+        else left = mid+1;
+    }
+    return left;
+}
