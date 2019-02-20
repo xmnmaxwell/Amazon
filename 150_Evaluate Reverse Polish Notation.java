3ce1281a70a75bc9a0cf887d50065ea71b087867
xmn880819 <xmn880819@tamu.edu>
2019-02-19 18:29:09 -0600

    Create 150_Evaluate Reverse Polish Notation.java

---
+++ 150_Evaluate Reverse Polish Notation.java
@@ -0,0 +1,27 @@
+Input: ["2", "1", "+", "3", "*"]
+Output: 9
+Explanation: ((2 + 1) * 3) = 9
+*************************************************
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens){
+            if (token.equals("+")){
+                stack.push(stack.pop() + stack.pop());
+            } else if (token.equals("-")){
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a - b);
+            } else if (token.equals("*")){
+                stack.push(stack.pop() * stack.pop());
+            } else if (token.equals("/")){
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(a / b);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.pop();
+    }
+}
