3bbaa725c164e1458967ef25f269f49eeb82f75b
xmn880819 <xmn880819@tamu.edu>
2019-02-21 17:49:04 -0600

    Update 279_perfect square.java

--- 279_perfect square.java
+++ 279_perfect square.java
@@ -1,11 +1,12 @@
 Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+    O(n2)
 ********************************************
 class Solution {
     public int numSquares(int n) {
         int[] dp = new int[n+1];
         Arrays.fill(dp, Integer.MAX_VALUE);
         for (int i = 0; i * i <= n; i++){
-            dp[i*i] = 1;
+            dp[i*i] = 1; // for all the square numbers, their dp is 1. this is to set boundary condition.
         }
         for (int i = 0; i <= n; i++){
             for (int j = 1; j*j <= i; j++){
