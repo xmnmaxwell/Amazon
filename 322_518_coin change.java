77713b3a55f94ba17bb805da2ae29ef054519452
xmn880819 <xmn880819@tamu.edu>
2019-02-21 15:28:25 -0600

    Update 322_518_coin change.java

--- 322_518_coin change.java
+++ 322_518_coin change.java
@@ -7,24 +7,19 @@ In the worst case the recursive tree of the algorithm has height of SS and the a
 because it caches precalculated solutions in a table. Each subproblem is computed with nn iterations, one by coin denomination. 
 *********************************************************
 public class Solution {
-
-    public int coinChange(int[] coins, int amount) {        
-        if (amount < 1) return 0;
-        return coinChange(coins, amount, new int[amount]);
-    }
-
-    private int coinChange(int[] coins, int rem, int[] count) {
-        if (rem < 0) return -1;
-        if (rem == 0) return 0;
-        if (count[rem - 1] != 0) return count[rem - 1];
-        int min = Integer.MAX_VALUE;
-        for (int coin : coins) {
-            int res = coinChange(coins, rem - coin, count);
-            if (res >= 0 && res < min)
-                min = 1 + res;
+    public int coinChange(int[] coins, int amount) {
+        int max = amount + 1;             
+        int[] dp = new int[amount + 1];  
+        Arrays.fill(dp, max);  
+        dp[0] = 0;   
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
         }
-        count[rem - 1] = (min == Integer.MAX_VALUE) ? -1 : min;
-        return count[rem - 1];
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
 **********************************************************************************
@@ -42,7 +37,6 @@ Once you figure out all these, it's easy to write out the code:
     public int change(int amount, int[] coins) {
         int[][] dp = new int[coins.length+1][amount+1];
         dp[0][0] = 1;
-        
         for (int i = 1; i <= coins.length; i++) {
             dp[i][0] = 1;
             for (int j = 1; j <= amount; j++) {
