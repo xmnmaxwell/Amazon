e17696a9f1bc75fcdb50567cc538734d58b82c70
xmn880819 <xmn880819@tamu.edu>
2019-02-22 14:07:22 -0600

    Create 76_minimum window substring.java

---
+++ 76_minimum window substring.java
@@ -0,0 +1,45 @@
+Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
+Input: S = "ADOBECODEBANC", T = "ABC"
+Output: "BANC"
+***********************************************************************************
+public class Solution {
+    public String minWindow(String s, String t) {
+        if(t.length()> s.length()) return "";
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : t.toCharArray()){
+            map.put(c, map.getOrDefault(c,0) + 1);
+        }
+        int counter = map.size();
+        
+        int begin = 0, end = 0;
+        int head = 0;
+        int len = Integer.MAX_VALUE;
+        
+        while(end < s.length()){
+            char c = s.charAt(end);
+            if( map.containsKey(c) ){
+                map.put(c, map.get(c)-1);
+                if(map.get(c) == 0) counter--;
+            }
+            end++;
+            
+            while(counter == 0){
+                char tempc = s.charAt(begin);
+                if(map.containsKey(tempc)){
+                    map.put(tempc, map.get(tempc) + 1);
+                    if(map.get(tempc) > 0){
+                        counter++;
+                    }
+                }
+                if(end-begin < len){
+                    len = end - begin;
+                    head = begin;
+                }
+                begin++;
+            }
+            
+        }
+        if(len == Integer.MAX_VALUE) return "";
+        return s.substring(head, head+len);
+    }
+}
